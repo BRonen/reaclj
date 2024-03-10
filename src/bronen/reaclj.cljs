2972e4f07e7957240bf1d7d7459fb02c59e09adb
Brenno Rodrigues <brennopereira6@gmail.com>
2024-03-10 20:47:04 +0000

    feat: initial reactive rendering

--- src/bronen/reaclj.cljs
+++ src/bronen/reaclj.cljs
@@ -1,11 +1,54 @@
 (ns bronen.reaclj)
 
-(defn greet
-  "Callable entry point to the application."
-  []
-  (js/console.log (str "Hello, Wworld!wwww")))
+(defn helloworldComponent
+  [state setState]
+  (let [examplename (:name @state)
+        counter (:counter @state)]
+    {:tag "p"
+     :children ["wasdwasd"
+                "wadawd"
+                (str counter)
+                (fn [_] {:tag "p"
+                         :on-click (fn [] (println "wasdwasd" @state)
+                                     (setState {:name examplename :counter (+ counter 1)} setState))
+                         :children [examplename]})]}))
+
+(defn parseTextNode
+  "Parses raw text into text content"
+  [node]
+  (let [frag (js/document.createDocumentFragment)]
+    (-> frag (.-textContent) (set! node))
+    frag))
+
+(defn parseNode
+  "Renders a Virtual DOM tree on the DOM"
+  [vnode state setState]
+  (let [node (vnode state setState)
+        el (-> node :tag js/document.createElement)
+        parseEl #(if (string? %) (parseTextNode %) (parseNode % state setState))]
+    (mapv #(.appendChild el (parseEl %)) (:children node))
+    (when (:on-click node)
+      (-> el (.-onclick) (set! (:on-click node))))
+    el))
+
+(defn renderDOM
+  "Renders a Virtual Node on the DOM"
+  [target el initialstate]
+  (let [elref (atom nil)
+        state (atom initialstate)
+        setState (fn [newState newSetState]
+                   (.removeChild target @elref)
+                   (reset! state newState)
+                   (reset! elref (parseNode el state newSetState))
+                   (.appendChild target @elref))]
+    
+    (reset! elref (parseNode el state setState))
+    (.appendChild target @elref)))
 
 (defn ^:export init
-  "I don't do a whole lot ... yet."
   []
-  (greet))
+  (let [state {:name "initial" :counter 0}]
+    (renderDOM
+     (js/document.getElementById "root")
+     helloworldComponent
+     state)))
--- test/bronen/reaclj_spec.cljs
+++ test/bronen/reaclj_spec.cljs
@@ -1,8 +1,6 @@
 (ns bronen.reaclj-spec
   {:dev/always true}
-  (:require [cljs.test :refer (deftest is)]
-            [bronen.reaclj :refer (greet)]))
+  (:require [cljs.test :refer (deftest is)]))
 
 (deftest a-failing-test
-  (greet)
   (is (= 1 (- 2 1))))

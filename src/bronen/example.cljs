5b1c44b8827ae8215655dde06d46ca41ad87e16c
Brenno Rodrigues <brennopereira6@gmail.com>
2024-03-11 02:50:35 +0000

    feat: new layout to sample app

--- src/bronen/example.cljs
+++ src/bronen/example.cljs
@@ -6,16 +6,15 @@
   (let [examplename (:name @state)
         counter (:counter @state)]
     {:tag "p"
-     :children ["wasdwasd"
-                "wadawd"
-                (str counter)
-                (fn [_] {:tag "p"
+     :children [(str "counter: " counter)
+                (fn [_] {:tag "br" :chidren []})
+                (fn [_] {:tag "button"
                          :on-click (fn [] (setState {:name examplename :counter (+ counter 1)} setState))
                          :children [examplename]})]}))
 
 (defn ^:export init
   []
-  (let [state {:name "initial" :counter 0}]
+  (let [state {:name "increment" :counter 0}]
     (renderDOM
      (js/document.getElementById "root")
      helloworldComponent
